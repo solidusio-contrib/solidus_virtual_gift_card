ea4b7ea06564745094df88b29c2a6f8c523fb976
Phillip Birtcher <pbirtcher@bonobos.com>
2014-06-11 12:10:24 -0400

    Make store credits a has many association on types

--- app/models/spree/store_credit_type.rb
+++ app/models/spree/store_credit_type.rb
@@ -1,6 +1,6 @@
 module Spree
   class StoreCreditType < ActiveRecord::Base
     DEFAULT_TYPE_NAME = 'Promotional'
-    has_one :store_credit, class_name: 'Spree::StoreCredit'
+    has_many :store_credits, class_name: 'Spree::StoreCredit', foreign_key: 'type_id'
   end
 end
