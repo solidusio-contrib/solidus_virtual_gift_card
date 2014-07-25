055db16b9b0131ceaf26a8ff181d5c3008e859dc
Magnus von Koeller <hello@magnusvk.com>
2014-07-25 18:53:30 -0400

    Safer way to generate gift card codes
    The current piece of code generated a 15-character code for us once.
    Also, I have no idea how it works. This seems safer.
    
    We can't run the tests here right now because it won't let me bundle
    install, so... Monday.
    
    Paired with @plongyear on this.

--- app/models/concerns/spree/redemption_code_generator.rb
+++ app/models/concerns/spree/redemption_code_generator.rb
@@ -1,6 +1,7 @@
 module Spree::RedemptionCodeGenerator
   def self.generate_redemption_code
-    rand(36**16).to_s(36).upcase
+    chars = [('A'..'Z'), ('0'..'9')].map(&:to_a).flatten
+    16.times.map { chars[rand(chars.count)] }.join
   end
 
   def self.format_redemption_code_for_lookup(redemption_code)
