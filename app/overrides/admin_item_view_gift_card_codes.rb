7101fbef09deecc07620e19c52e55754e6a07843
exsuzme <suzjacobson2@gmail.com>
2015-07-31 16:02:56 -0400

    Also deface override for cartons

--- app/overrides/admin_item_view_gift_card_codes.rb
+++ app/overrides/admin_item_view_gift_card_codes.rb
@@ -4,3 +4,10 @@ Deface::Override.new(
   insert_bottom: ".item-name",
   partial: "spree/admin/orders/gift_card_redemption_codes",
 )
+
+Deface::Override.new(
+  virtual_path: "spree/admin/orders/_carton_manifest",
+  name: "admin_item_view_gift_card_codes",
+  insert_bottom: ".item-name",
+  partial: "spree/admin/orders/gift_card_redemption_codes",
+)
