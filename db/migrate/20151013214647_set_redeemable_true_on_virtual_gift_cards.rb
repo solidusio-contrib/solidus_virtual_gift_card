71055fd5b85d0c31fbc5aa8ea7dd2f5427d54e10
Phillip Birtcher <philbirt@philbirt.com>
2015-10-15 13:23:58 -0400

    Add bang to update attributes in redeemable migration

--- db/migrate/20151013214647_set_redeemable_true_on_virtual_gift_cards.rb
+++ db/migrate/20151013214647_set_redeemable_true_on_virtual_gift_cards.rb
@@ -1,7 +1,7 @@
 class SetRedeemableTrueOnVirtualGiftCards < ActiveRecord::Migration
   def up
     Spree::VirtualGiftCard.find_each do |gift_card|
-      gift_card.update_attributes(redeemable: true)
+      gift_card.update_attributes!(redeemable: true)
     end
   end
 
