74ed5de0497080d447eee3308fc1e9a5f4910948
Phillip Birtcher <philbirt@philbirt.com>
2015-10-29 10:43:46 -0400

    Add database checks to gift card feature spec

--- spec/features/admin/gift_cards_spec.rb
+++ spec/features/admin/gift_cards_spec.rb
@@ -12,6 +12,10 @@ describe 'Gift Cards', :type => :feature, :js => true do
   describe "edit gift card" do
     let(:gift_card) { create(:redeemable_virtual_gift_card) }
     let(:order) { gift_card.line_item.order }
+    let(:new_recipient_name) { "Heidi" }
+    let(:new_recipient_email) { "heidi@gmail.com" }
+    let(:new_purchaser_name) { "Neerali" }
+    let(:new_gift_message) { "Sweaters!" }
 
     it "can edit recipient information and send email date" do
       visit spree.cart_admin_order_path(order)
@@ -20,10 +24,10 @@ describe 'Gift Cards', :type => :feature, :js => true do
         click_link("Edit Details")
       end
 
-      fill_in "virtual_gift_card_recipient_name", with: "Heidi"
-      fill_in "virtual_gift_card_recipient_email", with:"heidi@gmail.com"
-      fill_in "virtual_gift_card_purchaser_name", with: "Neerali"
-      fill_in "virtual_gift_card_gift_message", with: "Sweaters!"
+      fill_in "virtual_gift_card_recipient_name", with: new_recipient_name
+      fill_in "virtual_gift_card_recipient_email", with: new_recipient_email
+      fill_in "virtual_gift_card_purchaser_name", with: new_purchaser_name
+      fill_in "virtual_gift_card_gift_message", with: new_gift_message
       fill_in "virtual_gift_card_send_email_at", with: Date.tomorrow
 
       # Just so the datepicker gets out of poltergeists way.
@@ -31,6 +35,10 @@ describe 'Gift Cards', :type => :feature, :js => true do
 
       click_on 'Update'
       expect(page).to have_content("Gift card updated!")
+      expect(gift_card.reload.recipient_name).to eq(new_recipient_name)
+      expect(gift_card.recipient_email).to eq(new_recipient_email)
+      expect(gift_card.purchaser_name).to eq(new_purchaser_name)
+      expect(gift_card.gift_message).to eq(new_gift_message)
     end
   end
 end
