a08a3d8dc7dafc71c085a9c490a9a83f1cbd7527
Adam Mueller <adam@stembolt.com>
2016-07-07 15:29:28 -0700

    Pass store to from_address helper

--- app/mailers/spree/gift_card_mailer.rb
+++ app/mailers/spree/gift_card_mailer.rb
@@ -4,6 +4,6 @@ class Spree::GiftCardMailer < Spree::BaseMailer
     @order = @gift_card.line_item.order
     send_to_address = @gift_card.recipient_email.presence || @order.email
     subject = "#{Spree::Store.current.name} #{Spree.t('gift_card_mailer.gift_card_email.subject')}"
-    mail(to: send_to_address, from: from_address, subject: subject)
+    mail(to: send_to_address, from: from_address(Spree::Store.current), subject: subject)
   end
 end
