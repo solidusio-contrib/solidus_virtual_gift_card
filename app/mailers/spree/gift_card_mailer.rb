3d311d32a06e1ba4b13be51b212a5334b531f724
Brian Christian <brchristian@gmail.com>
2019-05-30 12:05:23 -0400

    Use updated I18n syntax

--- app/mailers/spree/gift_card_mailer.rb
+++ app/mailers/spree/gift_card_mailer.rb
@@ -3,7 +3,7 @@ class Spree::GiftCardMailer < Spree::BaseMailer
     @gift_card = gift_card.respond_to?(:id) ? gift_card : Spree::VirtualGiftCard.find(gift_card)
     @order = @gift_card.line_item.order
     send_to_address = @gift_card.recipient_email.presence || @order.email
-    subject = "#{Spree::Store.current.name} #{Spree.t('gift_card_mailer.gift_card_email.subject')}"
+    subject = "#{Spree::Store.current.name} #{I18n.t('spree.gift_card_mailer.gift_card_email.subject')}"
     mail(to: send_to_address, from: from_address(Spree::Store.current), subject: subject)
   end
 end
