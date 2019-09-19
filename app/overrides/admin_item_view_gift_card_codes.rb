01cef20fbd0380195d4677e835a76c69e1a25dcd
Alessandro Desantis <desa.alessandro@gmail.com>
2019-09-19 13:32:05 +0200

    Fix admin_item_view_gift_card_codes override
    Thanks to https://github.com/solidusio-contrib/solidus_virtual_gift_card/pull/45.

--- app/overrides/admin_item_view_gift_card_codes.rb
+++ app/overrides/admin_item_view_gift_card_codes.rb
@@ -1,20 +1,20 @@
 Deface::Override.new(
-  virtual_path: 'spree/admin/orders/_line_items',
-  name: 'add_gift_cards_to_admin_line_items',
-  insert_bottom: '.line-item-name',
-  partial: 'spree/admin/orders/cart_gift_card_details'
+  virtual_path: 'spree/admin/orders/confirm/_shipment_manifest',
+  name: 'add_gift_cards_to_admin_confirm',
+  insert_bottom: '.item-name',
+  partial: 'spree/admin/orders/cart_gift_card_details',
 )
 
 Deface::Override.new(
   virtual_path: 'spree/admin/orders/_shipment_manifest',
   name: 'admin_item_view_gift_card_codes',
   insert_bottom: '.item-name',
-  partial: 'spree/admin/orders/shipments_gift_card_details'
+  partial: 'spree/admin/orders/shipments_gift_card_details',
 )
 
 Deface::Override.new(
   virtual_path: 'spree/admin/orders/_carton_manifest',
   name: 'admin_item_view_gift_card_codes',
   insert_bottom: '.item-name',
-  partial: 'spree/admin/orders/shipments_gift_card_details'
+  partial: 'spree/admin/orders/shipments_gift_card_details',
 )
