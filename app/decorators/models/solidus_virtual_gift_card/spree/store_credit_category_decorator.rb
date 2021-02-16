66473ba43968473aa98079ab39e2cd779cd0abe9
Alessandro Desantis <desa.alessandro@gmail.com>
2021-02-16 12:02:08 +0100

    Make decorators compliant with Zeitwerk

--- app/decorators/models/solidus_virtual_gift_card/spree/store_credit_category_decorator.rb
+++ app/decorators/models/solidus_virtual_gift_card/spree/store_credit_category_decorator.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
-Spree::StoreCreditCategory::GIFT_CARD_CATEGORY_NAME = 'Gift Card'
-Spree::StoreCreditCategory.non_expiring_credit_types |= [Spree::StoreCreditCategory::GIFT_CARD_CATEGORY_NAME]
+module SolidusVirtualGiftCard
+  module Spree
+    module StoreCreditCategoryDecorator
+      GIFT_CARD_CATEGORY_NAME = 'Gift Card'
+
+      def self.prepended(base)
+        base.non_expiring_credit_types |= [GIFT_CARD_CATEGORY_NAME]
+      end
+
+      ::Spree::StoreCreditCategory.prepend self
+    end
+  end
+end
