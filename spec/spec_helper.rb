b61bcce21aefa74192dd01745e1491c4df182d19
Alberto Vena <kennyadsl@gmail.com>
2021-02-19 19:10:05 +0100

    Load factories with the new method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,8 +15,8 @@ require 'solidus_dev_support/rspec/feature_helper'
 # in spec/support/ and its subdirectories.
 Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
 
-# Requires factories defined in lib/solidus_virtual_gift_card/factories.rb
-require 'solidus_virtual_gift_card/factories'
+# require 'solidus_virtual_gift_card/testing_support/factories'
+SolidusDevSupport::TestingSupport::Factories.load_for(SolidusVirtualGiftCard::Engine)
 
 RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
