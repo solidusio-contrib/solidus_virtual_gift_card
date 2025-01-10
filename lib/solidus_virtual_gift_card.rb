25b134cd199fd389e7558c71a797c09e149917a4
Daniele Palombo <387690+DanielePalombo@users.noreply.github.com>
2025-01-10 18:32:22 +0100

    Run "bundle exec solidus extension ."

--- lib/solidus_virtual_gift_card.rb
+++ lib/solidus_virtual_gift_card.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-require 'solidus_core'
-require 'solidus_support'
-
+require 'solidus_virtual_gift_card/configuration'
 require 'solidus_virtual_gift_card/version'
 require 'solidus_virtual_gift_card/engine'
 
