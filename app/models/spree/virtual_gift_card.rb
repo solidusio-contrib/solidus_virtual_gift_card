11cc9b83ccc96ce9bb7b84eff515ad39e7c14241
Daniele Palombo <387690+DanielePalombo@users.noreply.github.com>
2025-01-11 18:40:07 +0100

    bundle exec rubocop -a

--- app/models/spree/virtual_gift_card.rb
+++ app/models/spree/virtual_gift_card.rb
@@ -18,11 +18,11 @@ class Spree::VirtualGiftCard < Spree::Base
   scope :by_redemption_code, ->(redemption_code) { where(redemption_code: redemption_code) }
   scope :purchased, -> { where(redeemable: true) }
 
-  def self.ransackable_associations(auth_object = nil)
+  def self.ransackable_associations(_auth_object = nil)
     %w[line_item order]
   end
 
-  def self.ransackable_attributes(auth_object = nil)
+  def self.ransackable_attributes(_auth_object = nil)
     %w[redemption_code recipient_email sent_at send_email_at]
   end
 
@@ -54,7 +54,7 @@ class Spree::VirtualGiftCard < Spree::Base
   end
 
   def make_redeemable!(purchaser:, inventory_unit:)
-    update!(redeemable: true, purchaser: purchaser, inventory_unit: inventory_unit, redemption_code: (redemption_code || generate_unique_redemption_code))
+    update!(redeemable: true, purchaser: purchaser, inventory_unit: inventory_unit, redemption_code: redemption_code || generate_unique_redemption_code)
   end
 
   def deactivate
--- lib/generators/solidus_virtual_gift_card/install/install_generator.rb
+++ lib/generators/solidus_virtual_gift_card/install/install_generator.rb
@@ -20,8 +20,8 @@ module SolidusVirtualGiftCard
       end
 
       def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/solidus_virtual_gift_card\n", before: %r{\*/}, verbose: true # rubocop:disable Layout/LineLength
-        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/solidus_virtual_gift_card\n", before: %r{\*/}, verbose: true # rubocop:disable Layout/LineLength
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/solidus_virtual_gift_card\n", before: %r{\*/}, verbose: true
+        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/solidus_virtual_gift_card\n", before: %r{\*/}, verbose: true
       end
 
       def add_migrations
@@ -29,11 +29,11 @@ module SolidusVirtualGiftCard
       end
 
       def run_migrations
-        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask('Would you like to run the migrations now? [Y/n]')) # rubocop:disable Layout/LineLength
+        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask('Would you like to run the migrations now? [Y/n]'))
         if run_migrations
           run 'bin/rails db:migrate'
         else
-          puts 'Skipping bin/rails db:migrate, don\'t forget to run it!' # rubocop:disable Rails/Output
+          puts 'Skipping bin/rails db:migrate, don\'t forget to run it!'
         end
       end
     end
