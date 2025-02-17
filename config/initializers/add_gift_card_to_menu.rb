bc93d260e8d25ce181955762367768dce0e63f2a
Daniele Palombo <387690+DanielePalombo@users.noreply.github.com>
2025-02-17 12:57:55 +0100

    Load admin only when Spree::Backend is included

--- config/initializers/add_gift_card_to_menu.rb
+++ config/initializers/add_gift_card_to_menu.rb
@@ -1,24 +1,26 @@
 # frozen_string_literal: true
 
 Rails.application.config.to_prepare do
-  Spree::Backend::Config.configure do |config|
-    config.menu_items = config.menu_items.map do |item|
-      if item.label.to_sym == :users
-        # The API of the MenuItem class changes in Solidus 4.2.0
-        if item.respond_to?(:children)
-          unless item.children.any? { |child| child.label == :gift_cards }
-            item.children << Spree::BackendConfiguration::MenuItem.new(
-              label: :gift_cards,
-              condition: -> { can?(:display, Spree::VirtualGiftCard) },
-              url: -> { Spree::Core::Engine.routes.url_helpers.admin_gift_cards_path },
-              match_path: /gift_cards/
-            )
+  if defined?(Spree::Backend)
+    Spree::Backend::Config.configure do |config|
+      config.menu_items = config.menu_items.map do |item|
+        if item.label.to_sym == :users
+          # The API of the MenuItem class changes in Solidus 4.2.0
+          if item.respond_to?(:children)
+            unless item.children.any? { |child| child.label == :gift_cards }
+              item.children << Spree::BackendConfiguration::MenuItem.new(
+                label: :gift_cards,
+                condition: -> { can?(:display, Spree::VirtualGiftCard) },
+                url: -> { Spree::Core::Engine.routes.url_helpers.admin_gift_cards_path },
+                match_path: /gift_cards/
+              )
+            end
+          else
+            item.sections << :gift_cards
           end
-        else
-          item.sections << :gift_cards
         end
+        item
       end
-      item
     end
   end
 end
--- config/initializers/gift_card_store_credit_event_originator.rb
+++ config/initializers/gift_card_store_credit_event_originator.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.config.to_prepare do
-  Spree::Admin::StoreCreditEventsHelper.originator_links[Spree::VirtualGiftCard.to_s] = {
-    new_tab: true,
-    href_type: :line_item,
-    translation_key: 'admin.gift_cards.gift_card_originator'
-  }
+  if defined?(Spree::Backend)
+    Spree::Admin::StoreCreditEventsHelper.originator_links[Spree::VirtualGiftCard.to_s] = {
+      new_tab: true,
+      href_type: :line_item,
+      translation_key: 'admin.gift_cards.gift_card_originator'
+    }
+  end
 end
