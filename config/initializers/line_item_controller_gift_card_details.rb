fde734c280ee9ca7e51d9a20ffb3d8f85cc8f43c
Daniele Palombo <387690+DanielePalombo@users.noreply.github.com>
2025-02-17 15:42:04 +0100

    Use Spree::Config.line_item_comparison_hooks
    ref https://github.com/solidusio/solidus/pull/6050

--- config/initializers/line_item_controller_gift_card_details.rb
+++ config/initializers/line_item_controller_gift_card_details.rb
@@ -2,5 +2,9 @@
 
 Rails.application.config.to_prepare do
   Spree::PermittedAttributes.line_item_attributes << { options: [gift_card_details: [:recipient_name, :recipient_email, :gift_message, :purchaser_name, :send_email_at]] }
-  Spree::Order.line_item_comparison_hooks.add('gift_card_match')
+  if Spree::Config.respond_to?(:line_item_comparison_hooks)
+    Spree::Config.line_item_comparison_hooks << 'gift_card_match'
+  else
+    Spree::Order.line_item_comparison_hooks.add('gift_card_match')
+  end
 end
